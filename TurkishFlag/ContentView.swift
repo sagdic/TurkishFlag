6646f06c59f7d55cf2e8a86b539502ad146e23eb
Tayfun Sağdıç <tayfun256sagdic@gmail.com>
2023-10-29 17:50:49 +0300

    Initial Commit

---
+++ TurkishFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TurkishFlag
+//
+//  Created by Tayfun Sagdic on 29.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TurkishFlag/TurkishFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurkishFlagApp.swift
+//  TurkishFlag
+//
+//  Created by Tayfun Sagdic on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TurkishFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
